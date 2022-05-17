3336c6f9a5b5285d164ed46359e39819124b360b
Thiago Rocha <105082780+ThiagoRocha1@users.noreply.github.com>
2022-05-16 21:22:12 -0300

    Update Semi - Final

--- lista3thiagorocha.py
+++ lista3thiagorocha.py
@@ -68,8 +68,154 @@ def questao2 (minimo,maximo,m1,m2):
 
 
 
-#Questao3 
-def questao3 (pi,a,f,maxTentativa):
+##Questao3 
+
+def questao3 (pi,a,f,maxTentativas):
+
 # Essa funcao retorna, respectivamente, um inteiro representando o numero de tentativas # e um booleano que representa se a pessoa atingiu o alvo em ate o numero maximo de ten-# tativas. 
+
 # A funcao recebe como argumento, respectivamente, posicao inicial do peso, posicao do 
-# alvo, constante f e numero max de tentativas 
+
+# alvo, constante f e numero max de tentativas. O peso atinge o alvo caso a distancia final P (A-P) e o Alvo seja menor ou igual a 0.1 metros.
+
+    tentativas = 1
+
+    p = pi - ((f) * (pi-a))
+
+    deltaPA = p - a
+
+    
+
+    while abs(deltaPA) > 0.1 and tentativas < maxTentativas:
+
+        tentativas += 1
+
+        pi=p
+
+        p=pi-((f)*(pi-a))
+
+        deltaPA = p-a 
+
+        
+
+        
+
+    # Analisa o resultado do return da funcao 
+
+    if abs(deltaPA) <= 0.1 and tentativas <= maxTentativas:
+
+        return tentativas,True
+
+    elif abs(deltaPA)<= 0.1 and tentativas > maxTentativas:
+
+        return tentativas,False
+
+    else:
+
+        return tentativas,False
+
+print((questao3(10,5,1,50)))
+
+print((questao3(10,5,2,50)))
+
+print((questao3(0,2,0.5,5)))
+
+print((questao3(-2,3,0.1,40))) 
+
+#Questao4 
+
+def questao4(x):
+
+    i = 1
+
+    soma = 0
+
+    
+
+    while i < x:
+
+        if x%i == 0:
+
+            soma = soma + i
+
+            i += 1 
+
+        else:
+
+            i+= 1 
+
+    if soma == x:
+
+        return True
+
+    else:
+
+        return False 
+
+print(questao4(28))
+
+print(questao4(496))
+
+print(questao4(8))
+
+print(questao4(11))
+
+#Questao 5 
+
+def questao5a (x):
+
+    # Retorna o quociente e resto da divisao por 10 
+
+    q = x//10 
+
+    r = x%10
+
+    
+
+    return q, r
+
+    
+
+def questao5b(x,y):
+
+    contagem = 0
+
+    q,r = questao5a(x)
+
+    while q > 0:
+
+        if r == y:
+
+            contagem += 1
+
+            q = q/10
+
+        elif q != y:
+
+            contagem = contagem
+
+            q= q/10
+
+    
+
+    return contagem
+
+#5A
+
+print(questao5a(3296))
+
+print(questao5a(8765421))
+
+print(questao5a(10101))
+
+#5B
+
+print(questao5b(934172665,6))
+
+print(questao5b(4544660,3))
+
+print(questao5b(111223,1))
+
+print(questao5b(17970747,7))
+
+print(questao5b(765,5))
